67d70e2d9d28b54b275e70e4b51035d41d3331d9
everett <edmoss@loyola.edu>
2022-11-21 11:34:40 -0500

    removed done boolean

--- server/index.js
+++ server/index.js
@@ -8,6 +8,31 @@ const cheerio = require('cheerio')
 const app = express()
 app.use(cors())
 
+//constants
+const params = {
+    'client_id': '8m4K5d2x4mNmUHLhLmsGq9vxE3dDkxCm',
+    'limit': '20',
+    'offset': '0',
+    'linked_partitioning': '1',
+    'app_version': '1668781563',
+    'app_locale': 'en'
+}
+
+const headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Origin': 'https://soundcloud.com',
+    'Referer': 'https://soundcloud.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"'
+}
+
 app.get('/', (req, res) => {
     const data = {
         name: 'test',
@@ -46,85 +71,64 @@ app.get('/likes', async (req, res) => {
 })
 
 app.get('/tracks', async (req, res) => {
+    //where all track data is stored
     tracks = []
 
     //get id
     const id = await getID('everettbeats')
 
     //create url
-    const url = `https://api-v2.soundcloud.com/users/${id}/tracks`
+    var baseURL = 'https://api-v2.soundcloud.com/users/'
+    const url = baseURL + `${id}/tracks`
 
     //get tracks json
-    const response = await axios.get(url, {
-        params: {
-            'representation': '',
-            'client_id': '8m4K5d2x4mNmUHLhLmsGq9vxE3dDkxCm',
-            'limit': '20',
-            'offset': '0',
-            'linked_partitioning': '1',
-            'app_version': '1668781563',
-            'app_locale': 'en'
-        },
-        headers: {
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Origin': 'https://soundcloud.com',
-            'Referer': 'https://soundcloud.com/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"'
-        }
+    var response = await axios.get(url, {
+        params: params,
+        headers: headers
     });
 
     // store data from 1st page
     const firstPage = response.data
     var nextURL = firstPage.next_href
+    var currentPage = firstPage.collection
 
-    //console.log(firstPage.collection[0].playback_count)
+    while (nextURL != null) {
+        for (let index = 0; index < currentPage.length; index++) {
+            //assign wanted data to variables
+            const plays = currentPage[index].playback_count
+            const track_title = currentPage[index].title
+            const genre = currentPage[index].genre
+            const comments = currentPage[index].comment_count
+            const artwork = currentPage[index].artwork_url
+            const artist_name = currentPage[index].user.username
 
-    const firstCollection = firstPage.collection
+            //add track data to array
+            tracks.push({
+                title: track_title,
+                artist: artist_name,
+                plays: plays,
+                comments: comments,
+                genre: genre,
+                artwork, artwork
+            })
+        }
 
-    for (let index = 0; index < firstCollection.length; index++) {
-        const plays = firstPage.collection[index].playback_count
-        const track_title = firstPage.collection[index].title
-        const genre = firstPage.collection[index].genre
-        const comments = firstPage.collection[index].comment_count
-        const artwork = firstPage.collection[index].artwork_url
-        const artist_name = firstPage.collection[index].user.username
+        if (baseURL != nextURL) {
+            baseURL = nextURL
 
-        tracks.push({
-            title: track_title,
-            artist: artist_name,
-            plays: plays,
-            comments: comments,
-            genre: genre,
-            artwork, artwork   
-        })
-        
+            var response = await axios.get(baseURL, {
+                params: params,
+                headers: headers
+            });
+            const newPage = response.data
+            currentPage = newPage.collection
+
+            nextURL = newPage.next_href
+        }
+
     }
+    console.log(tracks.length)
 
-    console.log(tracks)
-    
-
-
-/* 
-tracks.push({
-            title: track_title,
-            artist: artist_name,
-            plays: plays,
-            comments: comments,
-            genre: genre,
-            artwork, artwork   
-        })
-*/
-
-
-    
 })
 
 const getID = async (accountName) => {
@@ -140,7 +144,7 @@ const getID = async (accountName) => {
             const trimmedStart = idJSON.substring(24) //trims before '='
             const accountObj = JSON.parse(trimmedStart.slice(0, -1)) //trims ';' & turns to object
 
-            id = accountObj[6].data.id
+            id = accountObj[6].data.id //this is where id is located
         }
     })
     return id;
